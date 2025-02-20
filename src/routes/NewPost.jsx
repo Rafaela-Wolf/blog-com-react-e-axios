bd77d71b620f6199d7254c7bad6a68d4fd64fdb2
Rafaela Wolf <rafaelawolf52@gmail.com>
2025-02-20 10:40:18 -0300

    adicionando a página de newpost e colocando os estilos iniciais nela

--- src/routes/NewPost.jsx
+++ src/routes/NewPost.jsx
@@ -1,8 +1,30 @@
-
+import './NewPost.css';
 
 const NewPost = () => {
   return (
-    <div>NewPost</div>
+    <div>
+      <h2>Inserir novo post:</h2>
+      <form>
+        <div className="form-control">
+          <label htmlFor="title">Título:</label>
+          <input 
+            type="text" 
+            name="title"
+            id="title"
+            placeholder="Digite o título"
+          />
+        </div>
+        <div className="form-control">
+          <label htmlFor="body">Conteúdo:</label>
+          <textarea  
+            name="body"
+            id="body"
+            placeholder="Digite o conteúdo"
+          />
+        </div>
+        <input type="submit" value="Criar post" className="btn" />
+      </form>
+    </div>
   )
 }
 
